4b1eea1ea4e3138b7f3162c17688a8a3392bbcf4
Stormfireuttam <uttam@MILE-BL-3968-LAP.localdomain>
2023-02-22 11:17:52 +0530

    formatted movies data

---
+++ assessments/bigdata-assessment-01/python-script-movies.py
@@ -0,0 +1,24 @@
+import csv
+
+with open('/home/uttam/futurense-datengg-bootcamp/assessments/bigdata-assessment-01/movies.csv', 'r') as file:
+    f=open("movies1.csv",'a+')
+    csvreader = csv.reader(file)
+    idx = 0
+    output_values=[1,2,3,4]
+    for line in csvreader:
+        values=line
+        if(idx == 0):
+            idx += 1
+            continue
+        output_values[0]=values[0]
+        if(len(values[1].split("(")) > 1):
+            output_values[1]=values[1].split("(")[0]
+            output_values[2]=values[1].split("(")[1][:-1]
+        else:
+            output_values[1]=values[1]
+            output_values[2]=""
+        output_values[3]=values[2]
+        record="#".join(output_values)
+        f.write(record)
+    f.close()
+
---
+++ assessments/bigdata-assessment-01/ques02.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python3 python-script-movies.py
+
+
