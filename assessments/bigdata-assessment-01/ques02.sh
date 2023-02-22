94fe962e39a20b862672f08b2da8d1087cbe86b1
Uttam Mittal <uttam29mittal@gmail.com>
2023-02-22 16:39:23 +0530

    Update ques02.sh

--- assessments/bigdata-assessment-01/ques02.sh
+++ assessments/bigdata-assessment-01/ques02.sh
@@ -4,5 +4,6 @@ python3 python-script-movies.py
 
 start-all.sh
 
-hdfs dfs -put '/home/uttam/futurense-datengg-bootcamp/assessments/bigdata-assessment-01/movies1.csv' '/user/training/movie'
-hdfs dfs -put '/home/uttam/futurense-datengg-bootcamp/assessments/bigdata-assessment-01/ratings.csv' '/user/training/movie'
+hdfs dfs -mkdir /user/training/movie
+hdfs dfs -put /home/uttam/futurense-datengg-bootcamp/assessments/bigdata-assessment-01/movies1.csv /user/training/movie/movies1.csv
+hdfs dfs -put /home/uttam/futurense-datengg-bootcamp/assessments/bigdata-assessment-01/ratings.csv /user/training/movie/ratings1.csv
