9f11eb05d8bdbb9d5d82b16451399335c61b0eb8
Uttam Mittal <uttam29mittal@gmail.com>
2023-02-24 16:23:12 +0530

    Update and rename 006 - Union and Distinct.py to 006 - Union,Intersection,Distinct.py

--- labs/pyspark/006 - Union,Intersection,Distinct.py
+++ labs/pyspark/006 - Union,Intersection,Distinct.py
@@ -8,8 +8,19 @@
 >>> rdd3.collect()
 [1, 2, 3, 4, 5, 5, 6, 7, 8, 9]
 >>> rdd3.distinct().collect()           # Wide Transformation
-[1, 2, 3,
+[1, 2, 3, 4, 5, 6, 7, 8, 9]
 >>> rdd3.getNumPartitions()
 16
 >>> rdd1.getNumPartitions()
 8
+>>> rdd4 = rdd1.intersection(rdd2)      # Wide Transformation
+>>> rdd4.collect()
+[5]
+>>> rdd1.getNumPartitions()
+8
+>>> rdd2.getNumPartitions()
+8
+>>> rdd3.getNumPartitions()
+16
+>>> rdd4.getNumPartitions()
+16
