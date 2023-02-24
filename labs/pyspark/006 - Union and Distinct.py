83db39a60674b42cf12f3e0375234823fe7faf0e
Uttam Mittal <uttam29mittal@gmail.com>
2023-02-24 16:13:40 +0530

    Create 006 - Union and Distinct.py

---
+++ labs/pyspark/006 - Union and Distinct.py
@@ -0,0 +1,11 @@
+>>> rdd1 = sc.parallelize([1,2,3,4,5])
+>>> rdd2 = sc.parallelize([6,7,8,9])
+>>> rdd3 = rdd1.union(rdd2)
+>>> rdd3.collect()
+[1, 2, 3, 4, 5, 6, 7, 8, 9]
+>>> rdd2 = sc.parallelize([5,6,7,8,9])
+>>> rdd3 = rdd1.union(rdd2)             # Narrow Transformation
+>>> rdd3.collect()
+[1, 2, 3, 4, 5, 5, 6, 7, 8, 9]
+>>> rdd3.distinct().collect()           # Wide Transformation
+[1, 2, 3,
