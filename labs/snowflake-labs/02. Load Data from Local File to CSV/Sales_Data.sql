1d6179ed7e1ff1c8ba339d0866c3eb637abbbc27
Uttam Mittal <uttam29mittal@gmail.com>
2023-03-22 18:41:02 +0530

    Update Sales_Data.sql

--- labs/snowflake-labs/02. Load Data from Local File to CSV/Sales_Data.sql
+++ labs/snowflake-labs/02. Load Data from Local File to CSV/Sales_Data.sql
@@ -10,8 +10,10 @@ Units_Sold int,Unit_Price int,Unit_Cost int,Total_Revenue int,Total_Cost int,Tot
 
 -- Stage the data 
 put file://C:\Users\miles.MILE-BL-3968-LA\Downloads\Sales_Records.csv @MILES.PUBLIC.%sales;
+put file://C:\Users\miles.MILE-BL-3968-LA\Downloads\Sales_Records.csv @~/staged;
 
 -- Copy the data from stage into sales table
+COPY INTO sales FROM '@MILES.PUBLIC.%sales/Sales_Records.csv.gz' FILE_FORMAT=(TYPE=CSV, COMPRESSION = GZIP, SKIP_HEADER=1, FIELD_DELIMITER=',');
 COPY INTO sales FROM '@~/staged/Sales_Records.csv.gz' FILE_FORMAT=(TYPE=CSV, COMPRESSION = GZIP, SKIP_HEADER=1, FIELD_DELIMITER=',');
 
 -- View the data
